71fcf81994d60df02b919e4b3edb2e61c117f6f4
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:48:55 -0300

    feat(handler): handler global de erros

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,9 +1,14 @@
-import { AppError } from "../errors/errorHandler.js";
+import { AppError, NotFounError } from "../errors/errorHandler.js";
 
 export const errorMiddleware = (err, req, res, next) => {
     if(err instanceof AppError){
         return res.status(err.statusCode).json({message: err.message});
     }
 
+    if(err instanceof NotFounError){
+        return res.status(err.statusCode).json({message: err.message});
+    }
+
+    console.log(err);
     return res.status(500).json({message: "Erro interno do servidor, tente novamente mais tarde"});
 }
