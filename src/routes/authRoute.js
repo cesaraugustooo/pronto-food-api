7c4d98401510a3de87d71067807de3025d8026e0
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:56:01 -0300

    feat(route): adiciona rota para login

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,11 @@
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 import { Router } from "express";
 import { validated } from "../middlewares/validatedZod.js";
-import * as empresaSchema  from "../schemas/RegisterSchema.js"; 
+import * as empresaSchema  from "../dtos/authDTO.js"; 
 
 const app = Router()
 
 app.post('/register',validated(empresaSchema.createdSchema),register);
+app.post('/login',validated(empresaSchema.loginSchema),login);
 
 export { app }
