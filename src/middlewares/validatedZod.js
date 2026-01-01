7a37154f4e01532a2284d2cfefb3b995fdbc6ffb
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:54:47 -0300

    feat(middleware): middleware para validacao automatica de DTOS com Zod

--- src/middlewares/validatedZod.js
+++ src/middlewares/validatedZod.js
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const validated = (schema) => (req,res,next) => {
-    const result = schema.safeParse(req.body);
+    const result = schema.safeParse(req.body ?? {});
     
     if(!result.success){
         return res.status(400).json(result.error.format());
