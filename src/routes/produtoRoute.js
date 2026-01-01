606f042b0cf9e80939768104ec3061590ef817fd
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:56:21 -0300

    feat(route): Rotas para produtos

---
+++ src/routes/produtoRoute.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { verifyToken } from "../middlewares/auth/verifyToken.js"
+import { ownerMiddleware } from "../middlewares/empresa/empresaMiddleware.js";
+import { create, destroy, show, update } from "../controllers/produtoController.js";
+import { validated } from "../middlewares/validatedZod.js";
+import * as produtoSchema from "../dtos/produtoDTO.js";
+
+const app = Router();
+
+app.use(verifyToken);
+
+app.post('/:categoria',ownerMiddleware({compare: "categoria"}),validated(produtoSchema.createDTO),create);
+app.put('/:produto',ownerMiddleware({compare: "produto"}),validated(produtoSchema.updateDTO),update);
+app.delete('/:produto',ownerMiddleware({compare: "produto"}),destroy);
+app.get('/:produto',ownerMiddleware({compare: "produto"}),show);
+
+export { app }
