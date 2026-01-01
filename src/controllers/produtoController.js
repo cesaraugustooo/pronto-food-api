409c60e45f148891e54a2f25d153328c6012719d
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:50:13 -0300

    feat(controller): Controller para produtos

---
+++ src/controllers/produtoController.js
@@ -0,0 +1,56 @@
+import { createService, indexService, updateService, deleteService } from "../services/produtoService.js";
+
+export const index = async () => {
+
+}
+
+export const show = async (req,res,next) => {
+    try {
+        const produto = req.resource;
+
+        return res.json(produto)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const create = async (req,res,next) => {
+    try {
+        const empresa_id = req.user.empresa_id;
+        const categoria_id = req.resource.id;
+        const data = req.body;
+        
+        const produto = await createService({empresa_id, categoria_id, data})
+
+        return res.json(produto);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const update = async (req,res,next) => {
+    try {
+        const id = req.params.produto;
+        const data = req.body;
+    
+        const produto = await updateService({ id, data });
+    
+        return res.json(produto);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const destroy = async (req,res,next) => {
+    try {
+        const id = req.params.produto;
+
+        await deleteService({id});
+
+        return res.status(204).json({});
+
+    } catch (error) {
+        next(error)
+    }
+}
