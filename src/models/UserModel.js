8f6effbfad7c3933518e093a04a4d06989f592a4
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:55:35 -0300

    feat(model): Model para Users

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,3 +1,6 @@
+import { hashed } from "../services/bcryptService.js"; 
+
 export const create = async (prismaClient,data) => {
-    return await prismaClient.user.create({ data })
+    const password = await hashed(data.password);
+    return await prismaClient.user.create({ data:{...data,password: password } })
 }
