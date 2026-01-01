e410ccff3faac3d0c854e87895ec3566d7d373c4
Cesar <cesaraugusto23121989@gmail.com>
2026-01-01 19:40:16 -0300

    feat(routes): Novas rotas adicionadas

--- server/api.js
+++ server/api.js
@@ -1,8 +1,11 @@
 import express  from "express";
 import { Forcejson } from "../src/middlewares/forceJson.js";
 import { app as authRouter } from "../src/routes/authRoute.js";
+import { app as empresaRouter } from "../src/routes/empresaRouter.js";
+import { app as categoriaRouter } from "../src/routes/categoriaRoute.js";
+import { app as produtoRouter } from "../src/routes/produtoRoute.js";
 import { errorMiddleware } from "../src/middlewares/ErrorMiddleware.js";
-
+import { ownerMiddleware } from "../src/middlewares/empresa/empresaMiddleware.js";
 const app = express()
 
 // Middlewares Globais
@@ -13,6 +16,9 @@ app.use(Forcejson);
 // Rotas
 
 app.use('/auth',authRouter);
+app.use('/companys',empresaRouter);
+app.use('/categorias',categoriaRouter);
+app.use('/produtos',produtoRouter);
 
 //
 
