072d025c2c4a09a7cac06b74f7df5194c19d8a08
Cesar <cesaraugusto23121989@gmail.com>
2026-01-03 19:55:18 -0300

    feat(route): novo endpoint de listagem de todas categorias

--- src/routes/categoriaRoute.js
+++ src/routes/categoriaRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth/verifyToken.js"
 import { ownerMiddleware } from "../middlewares/empresa/empresaMiddleware.js";
-import { destroy, show, store, update } from "../controllers/categoriaController.js";
+import { destroy, index, show, store, update } from "../controllers/categoriaController.js";
 import { validated } from "../middlewares/validatedZod.js";
 import * as dto from "../dtos/categoriaDTO.js";
 
@@ -9,6 +9,7 @@ const app = Router();
 app.use(verifyToken)
 
 app.post('', validated(dto.createDTO), store);
+app.get('', index);
 app.get('/:categoria',ownerMiddleware({ compare: "categoria" }),show);
 app.put('/:categoria', ownerMiddleware({compare: "categoria"}), validated(dto.updateDTO), update);
 app.delete('/:categoria', ownerMiddleware({compare: "categoria"}), destroy);
