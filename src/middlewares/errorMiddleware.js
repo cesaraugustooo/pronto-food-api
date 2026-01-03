4aeb6e47ff8b9ec65f545a0d2c73d17c7a6e3091
Cesar <cesaraugusto23121989@gmail.com>
2026-01-03 19:54:16 -0300

    fix: corrigi erro de classe inexistente

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,11 +1,7 @@
-import { AppError, NotFounError } from "../errors/errorHandler.js";
+import { AppError } from "../errors/errorHandler.js";
 
 export const errorMiddleware = (err, req, res, next) => {
     if(err instanceof AppError){
-        return res.status(err.statusCode).json({message: err.message});
-    }
-
-    if(err instanceof NotFounError){
         return res.status(err.statusCode).json({message: err.message});
     }
 
