14c9c0b87379f46170494332e0f0296a950c213a
Cesar <cesaraugusto23121989@gmail.com>
2026-01-10 16:17:05 -0300

    feat(model): model para pedidos

--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -1,13 +1,32 @@
 import { prisma } from "../core/database.js";
 
 export const index = async ({ empresa_id, skip, take }) => {
-   
+
 }
 
 export const create = async ({ empresa_id, data }) => {
-   return prisma.empresa.create({data: {...data,empresa_id}})
+
+      const pedido = await prisma.$transaction(
+         async (tx) => {
+            const ultimo = await tx.pedido.findFirst({
+               where: { empresa_id },
+               orderBy: { numero: 'desc' }
+            })
+
+            const numero = (ultimo?.numero ?? 0) + 1
+
+            return tx.pedido.create({
+               data: { ...data, empresa_id, numero }
+            })
+         },
+         {
+            isolationLevel: 'Serializable'
+         }
+      )
+
 }
 
+
 export const show = async ({ id }) => {
-   
+
 }
