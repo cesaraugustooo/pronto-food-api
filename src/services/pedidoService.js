a68a491e870d1baa0fa4ccbc2bb4f76ec937b574
Cesar <cesaraugusto23121989@gmail.com>
2026-01-10 16:20:19 -0300

    feat: adiciona funcionalidade para criar pedidos, uso de transaction serializable para impedir race conditions

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -1,7 +1,21 @@
-import { show } from "../models/empresaModel";
-import { create } from "../models/pedidoModel"
+import { show } from "../models/empresaModel.js";
+import { create } from "../models/pedidoModel.js";
+import { AppError } from "../errors/errorHandler.js";
 
 export const storeService = async ({slug,data}) => {
-    const empresa = show(slug);
-    return await create({empresa_id: empresa.id, data});
+    const empresa = await show(slug);
+
+    if(!empresa){
+        throw new AppError("Empresa nao encontrada, tente novamente mais tarde.",404)
+    }
+
+    for(let i = 0; i < 3; i++){
+        try {
+            return await create({empresa_id: empresa.id, data});            
+        } catch (error) {
+            if(error.code !== "P2034"){
+                throw error
+            }
+        }
+    }
 } 
