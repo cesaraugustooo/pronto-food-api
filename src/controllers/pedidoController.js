621a029601dae0bea8da34b4a956e431867c856c
Cesar <cesaraugusto23121989@gmail.com>
2026-01-10 16:21:01 -0300

    feat: adiciona funcionalidade para criar pedidos

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,11 +1,11 @@
-import { storeService } from "../services/pedidoService"
+import { storeService } from "../services/pedidoService.js"
 
-export const store = (req,res,next) => {
+export const store = async (req,res,next) => {
     try {
         const slug = req.params.slug
         const data = req.body
 
-        const pedido = storeService({slug,data});
+        const pedido = await storeService({slug,data});
 
         return res.json(pedido);
     } catch (error) {
