0a6adf1eb82711571a4bf7337e8d7ccfdf9ecc25
Cesar <cesaraugusto23121989@gmail.com>
2026-01-13 23:12:16 -0300

    fix: valida coluna de preco faltante na tabela de pedidos

--- src/dtos/produtoDTO.js
+++ src/dtos/produtoDTO.js
@@ -1,12 +1,14 @@
-import { string, z } from "zod";
+import { float32, float64, string, z } from "zod";
 
 export const createDTO = z.object({
     nome: string().max(190),
     descricao: string().max(500).optional(),
+    preco: float64()
 }).strict()
 
 export const updateDTO = z.object({
     nome: string().max(190).optional(),
     descricao: string().max(500).optional(),
+    preco: float64().optional()
 }).strict()
 
