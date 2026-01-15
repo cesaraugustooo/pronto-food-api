ac35b0b272f0fc9fd633f3bdd5123a12306e1d76
Cesar <cesaraugusto23121989@gmail.com>
2026-01-15 00:54:33 -0300

    refactor: adiciona create de produtos do pedido na mesma transaction

--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -1,10 +1,11 @@
 import { prisma } from "../core/database.js";
+import { create as pedidoHasProdutoCreate } from "./pedidoHasProdutoModel.js"
 
 export const index = async ({ empresa_id, skip, take }) => {
 
 }
 
-export const create = async ({ empresa_id, data }) => {
+export const create = async ({ empresa_id, data, produtosData }) => {
 
       const pedido = await prisma.$transaction(
          async (tx) => {
@@ -15,9 +16,18 @@ export const create = async ({ empresa_id, data }) => {
 
             const numero = (ultimo?.numero ?? 0) + 1
 
-            return tx.pedido.create({
+            const pedido = await tx.pedido.create({
                data: { ...data, empresa_id, numero }
             })
+
+            const insertProduts = produtosData.map(produto => ({...produto, pedido_id: pedido.id}));
+
+            const pedidoHasProdutos = await pedidoHasProdutoCreate(insertProduts,tx);
+
+            return {
+               pedido: pedido,
+               produtos: pedidoHasProdutos
+            }
          },
          {
             isolationLevel: 'Serializable'
