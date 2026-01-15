bda0390cdf464c0b2f261ec7d891f630edff5c78
Cesar <cesaraugusto23121989@gmail.com>
2026-01-15 00:57:02 -0300

    refactor

--- src/dtos/pedidoDTO.js
+++ src/dtos/pedidoDTO.js
@@ -1,9 +1,15 @@
-import { float32, string, z } from "zod";
+import { array, float32, string, z } from "zod";
 
-export const pedidoCreateDTO = z.object({
-    nome_cliente: string().max(50),
-    telefone_cliente: string().max(12),
-    endereco_cliente: string().max(191),
-    observacoes_cliente:string().max(191),
-    total: float32()
-}).strict();
+export const pedidoCreateDTO =z.object({
+
+    pedido: z.object({
+        nome_cliente: string().max(50),
+        telefone_cliente: string().max(12),
+        endereco_cliente: string().max(191),
+        observacoes_cliente:string().max(191),
+        total: float32()
+    }).strict(),
+    produtos: z.object({
+        ids: array(z.string().uuid())
+    })
+}) 
